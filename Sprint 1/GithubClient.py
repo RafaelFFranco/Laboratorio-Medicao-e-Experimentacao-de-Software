77835e2c32072560b1e0760b26d25b9ba4301603
Paulo Assis <passis@sga.pucminas.br>
2026-08-11 20:40:34 -0300

    feat: ajusta codigo e adiciona env ao gitignore

---
+++ Sprint 1/GithubClient.py
@@ -0,0 +1,49 @@
+import os
+import json
+import requests
+
+class GithubClient:
+    URLBASE = "https://api.github.com/graphql"
+
+    def __init__(self):
+        auth_token = os.getenv("GITHUB_TOKEN")
+        self.headers = {
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": f"Bearer {auth_token}"
+        }
+
+    def getTopRepositories(self):
+        query = """
+            query {
+              search(query: "stars:>1 sort:stars-desc", type: REPOSITORY, first: 100) {
+                nodes {
+                  ... on Repository {
+                    nameWithOwner
+                    url
+                    description
+                    stargazerCount
+                    primaryLanguage {
+                      name
+                    }
+                    createdAt
+                  }
+                }
+              }
+            }
+        """
+        payload = {"query": query}
+        response = requests.post(url=GithubClient.URLBASE, headers=self.headers, json=payload).json()
+        if response.get("errors"):
+            print(f"Erro GQL: {response['errors'][0]['message']}")
+            return None
+        return response["data"]["search"]["nodes"]
+
+
+if __name__ == "__main__":
+    client = GithubClient()
+    repositories = client.getTopRepositories()
+
+    if repositories is not None:
+        with open("top100.json", "w", encoding="utf-8") as f:
+            json.dump(repositories, f, indent=2, ensure_ascii=False)
+        print(f"{len(repositories)} repositórios salvos em top100.json")
